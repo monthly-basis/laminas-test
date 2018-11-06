28d0bb04ce36020ed8a8417da867cde197ba020f
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-11-06 20:32:17 +0000

    Write preliminary module test case

---
+++ src/ModuleTestCase.php
@@ -0,0 +1,64 @@
+<?php
+namespace LeoGalleguillos\Test;
+
+use LeoGalleguillos\ProjectManagement\Module;
+use PHPUnit\Framework\TestCase;
+use Zend\Mvc\Application;
+
+class ModuleTestCase Extends TestCase
+{
+    protected function setUp()
+    {
+        $this->module = new static::Module();
+    }
+
+    public function testInstance()
+    {
+        $this->assertInstanceOf(Module::class, $this->module);
+    }
+
+    public function testGetConfig()
+    {
+        $applicationConfig = include($_SERVER['PWD'] . '/config/application.config.php');
+        $this->application = Application::init($applicationConfig);
+        $serviceConfig     = $this->module->getServiceConfig();
+        $serviceManager    = $this->application->getServiceManager();
+        $viewHelperManager = $serviceManager->get('ViewHelperManager');
+        $config            = $this->module->getConfig();
+
+        $this->assertTrue(is_array($config));
+
+        if (isset($config['view_helpers']['aliases'])) {
+            foreach ($config['view_helpers']['aliases'] as $alias => $class) {
+                $this->assertInstanceOf(
+                    $class,
+                    $viewHelperManager->get($class)
+                );
+            }
+        }
+
+        if (isset($config['view_helpers']['factories'])) {
+            foreach ($config['view_helpers']['factories'] as $class => $value) {
+                $this->assertInstanceOf(
+                    $class,
+                    $viewHelperManager->get($class)
+                );
+            }
+        }
+    }
+
+    public function testGetServiceConfig()
+    {
+        $applicationConfig = include($_SERVER['PWD'] . '/config/application.config.php');
+        $this->application = Application::init($applicationConfig);
+        $serviceConfig     = $this->module->getServiceConfig();
+        $serviceManager    = $this->application->getServiceManager();
+
+        foreach ($serviceConfig['factories'] as $class => $value) {
+            $this->assertInstanceOf(
+                $class,
+                $serviceManager->get($class)
+            );
+        }
+    }
+}
